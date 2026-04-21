a569316cab73a4f264b4b40071d500642a3173ae
Marco Pollacci <pollacci@gmail.com>
2026-04-21 13:47:37 +0200

    feat(project): bump version to 1.0.1 in package.json

--- main.d.ts
+++ main.d.ts
@@ -1,16 +1,4 @@
-type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
-
-type TZOffset = "Z" | `${"+" | "-"}${Digit}${Digit}:${Digit}${Digit}`;
-type Millis = "" | `.${Digit}${Digit}${Digit}`;
-
-type IsoTimeString =
-  `${Digit}${Digit}:${Digit}${Digit}:${Digit}${Digit}${Millis}${TZOffset}`;
-
-export type IsoDateString =
-  `${Digit}${Digit}${Digit}${Digit}-${Digit}${Digit}-${Digit}${Digit}`;
-
-export type IsoDateTimeString = `${IsoDateString}T${IsoTimeString}`;
-
-export declare function isChristmas(
-  today?: IsoDateString | IsoDateTimeString,
-): boolean;
+/**
+ * Need to be ISO date (with or without time)
+ */
+export declare function isChristmas(today?: string): boolean;
